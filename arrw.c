d94a93277aacf5ca02e7b586a50002ae8f2589cf
DenilBorda <denilbordasq@gmail.com>
2022-11-21 19:04:28 +0530

    21-11-22

--- arrw.c
+++ arrw.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
-#define n 5
-void dis(int a[])
+void arr(int a[5])
 {
-    for(int i=0;i<n; i++)
+    for(int i=0; i<=5; i++)
     {
         printf("\n%d",a[i]);
     }
 }
 int main()
 {
-    int a[n],i;
-    for(i=0; i<n; i++)
+    int a[5],i;
     {
         printf("Enter a[%d]",i);
         scanf("%d",&a[i]);
     }
-    dis(a);
+    arr(a);
 }
