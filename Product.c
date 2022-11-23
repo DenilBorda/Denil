60e1585ba4cefb5f936a4578a1001137b340d626
DenilBorda <denilbordasq@gmail.com>
2022-11-23 18:32:21 +0530

    23-11-22

--- Product.c
+++ Product.c
@@ -22,11 +22,14 @@ int main()
         printf("Enter qty :");
         scanf("%d",&a[i].qty);
     }
-    printf("id \tproduct \trate \tqty \tamt \tbillamt \tgst \tnetbill");
+    printf("id\tproduct\trate\tqty\tamt\tdis\tbillamt\tgst\tnetbill");
     for(i=0; i<2; i++)
     {
         a[i].amt = a[i].rate * a[i].qty;
         a[i].dis = a[i].amt * 0.05;
-        a[i]billamt
+        a[i].billamt + a[i].amt - a[i].dis;
+        a[i].gst = a[i].billamt * 0.18;
+        a[i].netbill = a[i].billamt + a[i].gst;
+        printf("\n%d\t%s\t%d\t%d\t%d\t%.2f\t%d\t%.2f\t%d",a[i].id,a[i].product,a[i].rate,a[i].qty,a[i].amt,a[i].dis,a[i].billamt,a[i].gst,a[i].netbill);
     }
 }
