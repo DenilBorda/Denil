d5fa4658c8deeb7e3fb75e315f72f5b99d7820c4
DenilBorda <denilbordasq@gmail.com>
2022-11-24 19:12:49 +0530

    24-11-22

---
+++ switchcase.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+    int main()
+{
+    int a,b,ch;
+    printf("Enter a: ");
+    scanf("%d",&a);
+    printf("Enter b: ");
+    scanf("%d",&b);
+
+    printf("\n1. Addition");
+    printf("\n2. Substraction");
+    printf("\n3. Multiplication");
+    printf("\n4. Division");
+    printf("\nEnter choice : ");
+    scanf("%d",&ch);
+    switch (ch)
+    {
+    case 1:
+            printf("\nAddition of a and b: %d",a+b);
+        break;
+    case 2: 
+            printf("\nSubstraction of a and b: %d",a-b);
+        break;
+    case 3:
+            printf("\nMultiplication of a and b: %d",a*b);
+        break;
+    case 4:
+            printf("\nDivision of a and b: %d",a/b);
+        break;
+    
+    default:
+            printf("\nWrong choice....");
+        break;
+    }
+
+}
